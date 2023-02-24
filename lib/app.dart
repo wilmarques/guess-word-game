03d03a886e044389a2309aceb7a45cb3efa35936
wilmarques <wileymarques@live.com>
2023-02-24 12:37:13 +0000

    Removing App State
    - Will add it later

--- lib/app.dart
+++ lib/app.dart
@@ -2,22 +2,19 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 import 'app_routes.dart';
-import 'app_state.dart';
 
 class GuessWordApp extends StatelessWidget {
   const GuessWordApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AppStateWidget(
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        useInheritedMediaQuery: true,
-        locale: DevicePreview.locale(context),
-        builder: DevicePreview.appBuilder,
-        routerConfig: routes,
-        title: 'Guess the Word',
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      routerConfig: routes,
+      title: 'Guess the Word',
     );
   }
 }
